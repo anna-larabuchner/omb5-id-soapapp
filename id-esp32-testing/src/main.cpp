fd71a0830f8a4a67d2716b34406c614bdc1a0a00
Anna-Lara Buchner <67783991+anna-larabuchner@users.noreply.github.com>
2024-01-05 14:18:36 +0100

    testing without blinking led
    same result as with previous code.

--- id-esp32-testing/src/main.cpp
+++ id-esp32-testing/src/main.cpp
@@ -76,7 +76,7 @@ void loop() {
   // check to see if it's time to blink the LED; that is, if the difference
   // between the current time and last time you blinked the LED is bigger than
   // the interval at which you want to blink the LED.
-  unsigned long currentMillis = millis();
+/*   unsigned long currentMillis = millis();
 
   if (currentMillis - previousMillis >= BLINK_INTERVAL) {
     // if the LED is off turn it on and vice-versa:
@@ -87,7 +87,7 @@ void loop() {
 
     // save the last time you blinked the LED
     previousMillis = currentMillis;
-  }
+  } */
 
   Serial.println(bleKeyboard.isConnected());
   bleKeyboard.print("Hello this is test");
